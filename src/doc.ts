aa4bcbb03c483ec4b404a44535ce9518fc98ad8b
James Bradlee <james@ihacks.dev>
2021-04-11 02:45:07 +0200

    removed FetchEvent interface

--- src/doc.ts
+++ src/doc.ts
@@ -5,24 +5,7 @@ import {
   processDiscordInteractions,
 } from "./utils.ts";
 
-/**
- * Deno Deploy dispatches a `FetchEvent` to your script when a new request is
- * made towards your deployment. The event, represented by the string `fetch`,
- * contains the details of the incoming request and methods to respond to the
- * same request.
- */
-interface FetchEvent extends Event {
-  /** Represents the incoming request to your deployment. */
-  readonly request: Request;
-
-  /**
-   * A method to respond to the incoming request.
-   * @param response The response to send back to the client.
-   */
-  respondWith(response: Promise<Response> | Response): void;
-}
-
-addEventListener("fetch", (evt: FetchEvent) => {
+addEventListener("fetch", (evt) => {
   const { request } = evt;
   try {
     if (isRequestByDiscordInteractions(request)) {
